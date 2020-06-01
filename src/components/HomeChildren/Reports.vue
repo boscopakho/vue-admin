ba1f67a0fbded609cf4c7f8feb3aade901b87e08
boscopakho <anny1007358818@qq.com>
2020-06-01 13:11:56 +0800

    report finished

--- src/components/HomeChildren/Reports.vue
+++ src/components/HomeChildren/Reports.vue
@@ -1,15 +1,71 @@
 <template>
-  <div>reports</div>
+  <div>
+     <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+        <div id="main" style="width: 600px;height:400px;"></div>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import _ from 'lodash'
+var echarts = require('echarts');
 export default {
   name: "reports",
   created() {},
   data() {
-    return {};
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    };
   },
-  methods: {}
+  methods: {
+     
+  },
+  async mounted(){
+    var myChart = echarts.init(document.getElementById('main'))
+
+    const {data:res}=await this.axios.get('reports/type/1')
+    var option=  _.merge(res.data,this.options)
+  // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+ }
+ 
 };
 </script>
 
